fc31d4e8c6b0107e41c1a8ccc731ae50038b819d
Mykyta Kizilov <53862785+mkizilov@users.noreply.github.com>
2024-07-09 16:22:44 -0500

    Adding IRF shift test

--- TCSPC-Reconvolution.py
+++ TCSPC-Reconvolution.py
@@ -6,28 +6,20 @@ from scipy.optimize import curve_fit, nnls
 from scipy.fft import ifft
 from tqdm import tqdm
 from scipy.signal import savgol_filter
+import os
 
 def txt_to_dataframe(filename):
-    # Find the index of the row that starts with "Time[ns]  No_of_photons"
     start_index = 0
-    end_index = 0
     with open(filename, 'r') as f:
         for i, line in enumerate(f):
             if "*BLOCK 1  ( Time[ns]  No_of_photons )" in line:
                 start_index = i + 1
-            # if "*END" in line:
-            #     end_index = i
-    data = pd.read_csv(filename, sep='\t', skiprows=start_index, skipfooter = end_index)
-    # Rename first column
+    data = pd.read_csv(filename, sep='\t', skiprows=start_index)
     data.rename(columns={data.columns[0]: 'Time Photons'}, inplace=True)
-    # Separate columns into two columns
     data[['Time', 'Photons']] = data['Time Photons'].str.split(expand=True)
-    # Remove first column
     data = data.drop(columns=['Time Photons'])
-    # Convert 'Photons' to numeric
     data['Photons'] = pd.to_numeric(data['Photons'], errors='coerce')
     data['Time'] = pd.to_numeric(data['Time'], errors='coerce')
-    # Remove Nans
     data = data.replace([np.inf, -np.inf], np.nan).dropna()
     data = data.sort_values(by='Time')
     return data
@@ -45,12 +37,9 @@ def create_dataframe_list(directory, file_startswith):
 def smooth_df(df, window_length=5, polyorder=3, title=None):
     df_smooth = df.copy()
     df_smooth['Photons'] = savgol_filter(df['Photons'], window_length=window_length, polyorder=polyorder)
-    #plot smoothed data and original data
     plt.plot(df['Time'], df['Photons'], label='Original Data')
     plt.plot(df['Time'], df_smooth['Photons'], label='Smoothed Data')
-    #set logy
     plt.yscale('log')
-    #set ylimit
     plt.ylim(0.1)
     if title:
         plt.title(title)
@@ -58,31 +47,32 @@ def smooth_df(df, window_length=5, polyorder=3, title=None):
     plt.show()
     return df_smooth
 
-def prepare_data(signal, irf, window_length=None, polyorder=None):
-    # Remove leading and trailing rows where 'Photons' is 0
+def prepare_data(df_signal, df_irf, window_length=None, polyorder=None):
+    signal = df_signal.copy()
+    irf = df_irf.copy()
     mask = signal['Photons'].ne(0)
     signal = signal.loc[mask.idxmax():mask[::-1].idxmax()]
-    mask_irf = irf['Photons'].ne(0)
-    irf = irf.loc[mask_irf.idxmax():mask_irf[::-1].idxmax()]
-    
-    # Make lengths equal
-    min_length = min(len(signal), len(irf))
-    signal = signal.iloc[:min_length]
-    irf = irf.iloc[:min_length]
-
-    # Smooth data
+    mask = irf['Photons'].ne(0)
+    irf = irf.loc[mask.idxmax():mask[::-1].idxmax()]
     if window_length and polyorder:
-        signal = smooth_df(signal, window_length=window_length, polyorder=polyorder, title='Signal')
-        irf = smooth_df(irf, window_length=window_length, polyorder=polyorder, title='IRF')
-        
+        signal['Photons'] = signal['Photons'].rolling(window=window_length, center=True).mean().fillna(method='bfill').fillna(method='ffill')
+        irf['Photons'] = irf['Photons'].rolling(window=window_length, center=True).mean().fillna(method='bfill').fillna(method='ffill')
+    # Shift to zero
+    signal['Time'] -= signal['Time'].min()
+    irf['Time'] -= irf['Time'].min()
+    # Make them same length
+    min_length = min(len(signal), len(irf))
+    signal = signal[:min_length]
+    irf = irf[:min_length]
     # Normalize IRF
-    irf['Photons'] = irf['Photons'] / irf['Photons'].sum()
+    irf['Photons'] /= irf['Photons'].sum()  # Normalize IRF
     return signal, irf
 
 def generate_gauss_irf(time, mu, sigma=0.01, A=10000):
     df_irf = pd.DataFrame()
     df_irf['Time'] = time
     df_irf['Photons'] = A * np.exp(-(time - mu)**2 / (2 * sigma**2))
+    df_irf['Photons'] /= df_irf['Photons'].sum()  # Normalize IRF
     return df_irf
 
 def convolve(x, h):
@@ -132,38 +122,14 @@ def get_weights(decay):
     weights = 1 / np.sqrt(decay + 1)
     return weights
 
-def prepare_data(df_signal, df_irf, window_length=None, polyorder=None):
-    signal = df_signal.copy()
-    irf = df_irf.copy()
-    
-    # Remove leading and trailing zeros
-    mask = signal['Photons'].ne(0)
-    signal = signal.loc[mask.idxmax():mask[::-1].idxmax()]
-
-    mask = irf['Photons'].ne(0)
-    irf = irf.loc[mask.idxmax():mask[::-1].idxmax()]
-    
-    # Smooth data if parameters are provided
-    if window_length and polyorder:
-        signal['Photons'] = signal['Photons'].rolling(window=window_length, center=True).mean().fillna(method='bfill').fillna(method='ffill')
-        irf['Photons'] = irf['Photons'].rolling(window=window_length, center=True).mean().fillna(method='bfill').fillna(method='ffill')
-    
-    return signal, irf
-
 def nnls_convol_irfexp(x_data, irf, p0, signal_values):
     irf_shift, *tau0 = p0
     shifted_irf = shift_irf(irf, irf_shift)
-    
     decays = [convolve(shifted_irf, exp_decay(x_data, tau)) for tau in tau0]
-    decays.append(np.ones_like(x_data))  # Adding ones for the offset component
-    
-    # Find the minimum length among the arrays
+    decays.append(np.ones_like(x_data))
     min_length = min(len(decays[0]), len(signal_values))
-    
-    # Trim all arrays to the minimum length
     decays_trimmed = [decay[:min_length] for decay in decays]
     signal_values_trimmed = signal_values[:min_length]
-    
     A = np.vstack(decays_trimmed).T
     x, _ = nnls(A, signal_values_trimmed)
     y = np.dot(A, x)
@@ -179,7 +145,14 @@ def plot_reconvolution_curve_fit(times, signal, fit, irf, irf_shift, taus, ampli
     times_irf = times[:min_length_irf]
     irf_adjusted = irf[:min_length_irf]
 
-    title_str = f"{plot_title}\nFitted IRF Shift: {np.round(irf_shift, 5)} ns\n Amplitudes {np.round(amplitudes, 5)} [photons]\n Taus {np.round(taus, 5)} [ns]\n $\chi^2$: {np.sum(residuals**2)/len(residuals)}"
+    # Scale the IRF to the signal
+    irf_adjusted *= np.sum(signal) / np.sum(irf_adjusted)
+    
+    title_str = (f"{plot_title}\nFitted IRF Shift: {np.round(irf_shift, 6)} channels\n"
+                 f"Amplitudes: {np.round(amplitudes, 6)} [photons]\n"
+                 f"Taus: {np.round(taus, 6)} [ns]\n"
+                 f"$\\chi^2_\\nu$: {np.round(np.sum((residuals**2)/(signal.size - len(taus) - 1)), 6)}")
+    
     fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, figsize=(12, 8))
     fig.tight_layout()
 
@@ -196,7 +169,7 @@ def plot_reconvolution_curve_fit(times, signal, fit, irf, irf_shift, taus, ampli
         ax[0].plot(times, amplitudes[i] * np.exp(-times / taus[i]), label=f'Exponent {i+1}', alpha=0.5, linewidth=3, linestyle='--')
 
     ax[0].set_xlim(times.min(), times.max())
-    ax[0].set_ylim(0.01)
+    ax[0].set_ylim(0.1)
     ax[0].legend()
     ax[0].set_yscale('log')
     ax[0].set_xlabel('Time (ns)')
@@ -214,7 +187,7 @@ def plot_reconvolution_curve_fit(times, signal, fit, irf, irf_shift, taus, ampli
         plt.savefig('plots/' + filename + '.png')
     plt.show()
 
-def reconvolution_curve_fit(df_signal, df_irf, exp_num=1, tau_bounds=(0, np.inf), smooth=None, num_splits=10, plot_title='Exponential Decay Reconvolution', filename=None):
+def reconvolution_curve_fit(df_signal, df_irf, exp_num=1, tau_bounds=None, smooth=None, num_splits=None, plot_title='Exponential Decay Reconvolution', filename=None):
     if smooth:
         window_length, polyorder = smooth
         signal, irf = prepare_data(df_signal, df_irf, window_length, polyorder)
@@ -223,29 +196,34 @@ def reconvolution_curve_fit(df_signal, df_irf, exp_num=1, tau_bounds=(0, np.inf)
     
     x_data = signal["Time"].values
     y_data = signal["Photons"].values
-    irf_values = irf["Photons"].values    
-    exp_num = range(exp_num)
+    irf_values = irf["Photons"].values
+
+    if tau_bounds is None:
+        tau_bounds = [(0, 1)] * exp_num
+    if num_splits is None:
+        num_splits = [10] * exp_num
     
     initial_irf_shift = find_irf_shift(y_data, irf_values)
-    
-    tau_grid = [np.linspace(tau_bounds[0], tau_bounds[1], num_splits) for _ in exp_num]
-    tau_combinations = np.array(np.meshgrid(*tau_grid)).T.reshape(-1, len(exp_num))
+
+    tau_grids = [np.linspace(tau_bounds[i][0], tau_bounds[i][1], num_splits[i]) for i in range(exp_num)]
+    tau_combinations = np.array(np.meshgrid(*tau_grids)).T.reshape(-1, exp_num)
     
     best_fit = None
     best_popt = None
     best_chi2 = np.inf
+
     with tqdm(total=len(tau_combinations), desc="Fitting progress") as pbar:
         for tau_comb in tau_combinations:
             p0 = [initial_irf_shift, *tau_comb]
             try:
                 weights = 1 / np.sqrt(y_data + 1)
-                popt, pcov = curve_fit(lambda x, *p: model_func(x, irf_values, p, y_data), x_data, y_data, p0=p0, bounds=([-len(x_data)] + [tau_bounds[0]]*len(exp_num), [len(x_data)] + [tau_bounds[1]]*len(exp_num)), sigma=weights)
+                popt, pcov = curve_fit(lambda x, *p: model_func(x, irf_values, p, y_data), x_data, y_data, p0=p0, bounds=([-len(x_data)] + [tau_bounds[i][0] for i in range(exp_num)], [len(x_data)] + [tau_bounds[i][1] for i in range(exp_num)]), sigma=weights)
                 _, _, y_fit = nnls_convol_irfexp(x_data, irf_values, popt, y_data)
                 residuals = y_data - y_fit
-                chi2 = np.sum((residuals / weights) ** 2)
+                chi2 = np.sum((residuals / weights) ** 2) / (y_data.size - len(popt))
                 if chi2 < best_chi2:
                     best_chi2 = chi2
-                    # best_fit = y_fit
+                    best_fit = y_fit
                     best_popt = popt
             except RuntimeError:
                 continue
@@ -267,36 +245,25 @@ def reconvolution_curve_fit(df_signal, df_irf, exp_num=1, tau_bounds=(0, np.inf)
     
     return best_popt, pcov
 
-def fit_model(mod, pars, signal, times, irf, weights, num_splits, bounds, model_type):
-    tau1_vals = np.linspace(bounds[model_type]['tau1'][0], bounds[model_type]['tau1'][1], num_splits)
-    tau2_vals = np.linspace(bounds[model_type]['tau2'][0], bounds[model_type]['tau2'][1], num_splits) if model_type in ['double', 'triple'] else [None]
-    tau3_vals = np.linspace(bounds[model_type]['tau3'][0], bounds[model_type]['tau3'][1], num_splits) if model_type == 'triple' else [None]
+def fit_model(mod, pars, signal, times, irf, weights, tau_bounds, num_splits, model_type, method='least_squares'):
+    tau_vals = [np.linspace(tau_bounds[i][0], tau_bounds[i][1], num_splits[i]) for i in range(len(tau_bounds))]
+    tau_combinations = np.array(np.meshgrid(*tau_vals)).T.reshape(-1, len(tau_bounds))
 
     best_result = None
     best_chi2 = np.inf
 
-    total_iterations = len(tau1_vals) * (len(tau2_vals) if tau2_vals[0] is not None else 1) * (len(tau3_vals) if tau3_vals[0] is not None else 1)
-
+    total_iterations = len(tau_combinations)
     with tqdm(total=total_iterations, desc='Fitting') as pbar:
-        for tau1_init in tau1_vals:
-            for tau2_init in (tau2_vals if tau2_vals[0] is not None else [None]):
-                for tau3_init in (tau3_vals if tau3_vals[0] is not None else [None]):
-                    taus = [tau1_init]
-                    if model_type in ['double', 'triple']:
-                        taus.append(tau2_init)
-                    if model_type == 'triple':
-                        taus.append(tau3_init)
-                    pars['ampl1'].set(value=np.max(signal))
-                    if model_type in ['double', 'triple']:
-                        pars['ampl2'].set(value=np.max(signal))
-                    if model_type == 'triple':
-                        pars['ampl3'].set(value=np.max(signal))
-                    pars['bg'].set(value=np.min(signal))
-                    result = mod.fit(signal, params=pars, x=times, irf=irf, weights=weights, method='least_squares')
-                    if result.redchi < best_chi2:
-                        best_result = result
-                        best_chi2 = result.redchi
-                    pbar.update(1)
+        for tau_comb in tau_combinations:
+            for i, tau_val in enumerate(tau_comb):
+                pars[f'tau{i+1}'].set(value=tau_val)
+                pars[f'ampl{i+1}'].set(value=np.max(signal))
+            pars['bg'].set(value=np.min(signal))
+            result = mod.fit(signal, params=pars, x=times, irf=irf, weights=weights, method=method)
+            if result.redchi < best_chi2:
+                best_result = result
+                best_chi2 = result.redchi
+            pbar.update(1)
     return best_result
 
 def plot_lmfit(times, signal, irf, result, model_type, plot_title, shift, filename=None):
@@ -304,8 +271,8 @@ def plot_lmfit(times, signal, irf, result, model_type, plot_title, shift, filena
     times_irf = times[:min_length_irf]
     irf_adjusted = irf[:min_length_irf]
 
-    title_str = (f"{plot_title}\n$R^2$: {np.round(result.rsquared, 2)}, $\\chi^2_\\nu$: {np.round(result.redchi, 2)}, "
-                f"$\\Delta$IRF: {np.round(shift, 2)}, $\\Delta bg$: {np.round(result.best_values['bg'], 2)}")
+    title_str = (f"{plot_title}\n$R^2$: {np.round(result.rsquared, 6)}, $\\chi^2_\\nu$: {np.round(result.redchi, 6)}, "
+                f"$\\Delta$IRF: {np.round(shift, 6)}, $\\Delta bg$: {np.round(result.best_values['bg'], 6)}")
 
     if model_type == 'single':
         title_str += (f", $A_1$: {np.round(result.best_values['ampl1'], 6)}, $\\tau_1$: {np.round(result.best_values['tau1'], 6)} ns")
@@ -339,7 +306,7 @@ def plot_lmfit(times, signal, irf, result, model_type, plot_title, shift, filena
                 'y-', label='Reconvolution', alpha=0.5, linewidth=4)
 
     ax[0].set_xlim(times.min(), times.max())
-    ax[0].set_ylim(0.01)
+    ax[0].set_ylim(0.1)
     ax[0].legend()
     ax[0].set_yscale('log')
     ax[0].set_xlabel('Time (ns)')
@@ -347,7 +314,7 @@ def plot_lmfit(times, signal, irf, result, model_type, plot_title, shift, filena
     residuals = (result.best_fit[:len(signal)] - signal)
     ax[1].plot(times, result.residual, 'r-', label='Weighted', alpha=1)
     ax[1].plot(times, residuals, 'b-', label='Residuals', alpha=0.5)
-    ax[1].set_ylim(-100, 100)
+    # ax[1].set_ylim(-100, 100)
     ax[1].set_xlim(times.min(), times.max())
     ax[1].set_xlabel("Time (ns)")
     ax[1].set_ylabel('Residuals')
@@ -355,9 +322,10 @@ def plot_lmfit(times, signal, irf, result, model_type, plot_title, shift, filena
     ax[0].legend()
     if filename:  
         plt.tight_layout()
-        plt.savefig(filename + '.png')
+        plt.savefig('plots/' + filename + '.png')
+    plt.show()
 
-def reconvolution_lmfit(df_signal, df_irf, model_type='double', bounds=None, smooth=None, num_splits=10, plot_title='Exponential Decay Reconvolution'):
+def reconvolution_lmfit(df_signal, df_irf, model_type='double', bounds=None, smooth=None, num_splits=None, plot_title='Exponential Decay Reconvolution', method='least_squares'):
     if smooth:
         window_length, polyorder = smooth
         signal, irf = prepare_data(df_signal, df_irf, window_length, polyorder)
@@ -367,10 +335,8 @@ def reconvolution_lmfit(df_signal, df_irf, model_type='double', bounds=None, smo
     x_data = signal["Time"].values
     y_data = signal["Photons"].values
     irf_values = irf["Photons"].values
-    irf_values /= np.sum(irf_values)
 
     initial_irf_shift = find_irf_shift(y_data, irf_values)
-    irf_values = np.roll(irf_values, initial_irf_shift)
     
     if bounds is None:
         bounds = {
@@ -393,8 +359,13 @@ def reconvolution_lmfit(df_signal, df_irf, model_type='double', bounds=None, smo
 
     pars['irf_shift'].set(value=initial_irf_shift)
 
+    if num_splits is None:
+        num_splits = [10] * len(bounds[model_type])
+
+    tau_bounds = [(bounds[model_type][key][0], bounds[model_type][key][1]) for key in bounds[model_type] if key.startswith('tau')]
+
     weights = get_weights(y_data)
-    best_result = fit_model(mod, pars, y_data, x_data, irf_values, weights, num_splits, bounds, model_type)
+    best_result = fit_model(mod, pars, y_data, x_data, irf_values, weights, tau_bounds, num_splits, model_type, method=method)
 
     if best_result:
         print(f"Optimal IRF shift: {best_result.best_values['irf_shift']}")
@@ -406,4 +377,3 @@ def reconvolution_lmfit(df_signal, df_irf, model_type='double', bounds=None, smo
         plot_lmfit(x_data, y_data, irf_values, best_result, model_type, plot_title, best_result.best_values['irf_shift'])
     
     return best_result, best_result.best_values
-
